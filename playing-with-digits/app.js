75df82ba9a7d157f1b72c71669cfdae6f5166172
Milo <milozhengz@gmail.com>
2019-07-15 09:41:16 +1000

    resolve challenge

--- playing-with-digits/app.js
+++ playing-with-digits/app.js
@@ -1,5 +1,16 @@
 function digPow(n, p) {
-  return -1
+  let digits = n
+    .toString()
+    .split("")
+    .map(digit => parseInt(digit))
+  let total = digits.reduce(function(accum, digit, i) {
+    return accum + Math.pow(digit, p+i)
+  }, 0)
+  if (total % n === 0) {
+    return total / n
+  } else {
+    return -1
+  }
 }
 
 module.exports = digPow
--- playing-with-digits/app.test.js
+++ playing-with-digits/app.test.js
@@ -4,4 +4,16 @@ describe("digPow", function() {
   it("should return -1 if such number k cannot be found", function(){
     expect(digPow(92, 1)).toBe(-1)
   });
+
+  it("should return 1 if k equals to 1", function(){
+    expect(digPow(89, 1)).toBe(1)
+  });
+
+  it("should return 2 if k equals to 2", function(){
+    expect(digPow(695, 2)).toBe(2)
+  });
+
+  it("should return 2=3 if k equals to 3", function(){
+    expect(digPow(46288, 3)).toBe(51)
+  });
 });
