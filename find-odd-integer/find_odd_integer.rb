ca0bae5d823b677e286b8c03daa083b083e06acc
Milo <milozhengz@gmail.com>
2019-07-17 11:02:16 +1000

    refactor code and log challenge

--- find-odd-integer/find_odd_integer.rb
+++ find-odd-integer/find_odd_integer.rb
@@ -1,9 +1,4 @@
 def find_it(seq)
-  results = []
-  seq.each do |elem|
-    if (seq.count(elem).odd?) && (elem.is_a? Integer)
-      results << elem
-    end
-  end
+  results = seq.select {|elem| (seq.count(elem).odd?) && (elem.is_a? Integer)}
   return results.first # assuming only one "odd times integer" in seq
 end
