cb0d1ba1cfb7049c615deda822fa68864da0bdac
Milo <milozhengz@gmail.com>
2019-07-17 10:53:04 +1000

    solve challenge with test passing

--- find-odd-integer/find_odd_integer.rb
+++ find-odd-integer/find_odd_integer.rb
@@ -1,3 +1,9 @@
 def find_it(seq)
-  return 9
+  results = []
+  seq.each do |elem|
+    if (seq.count(elem).odd?) && (elem.is_a? Integer)
+      results << elem
+    end
+  end
+  return results.first # assuming only one "odd times integer" in seq
 end
--- find-odd-integer/find_odd_integer_test.rb
+++ find-odd-integer/find_odd_integer_test.rb
@@ -9,4 +9,37 @@ class FindItTest < MiniTest::Test
   def test_return_single_elem_from_array
     assert_equal 9, find_it([9])
   end
+
+  def test_return_nil_if_elem_appears_twice
+    assert_equal nil, find_it([9, 9])
+  end
+
+  def test_return_elem_appearing_thrice
+    assert_equal 8, find_it([8, 0, 9, 9, 0, 8, 8])
+  end
+
+  def test_return_integer_appearing_odd_times
+    assert_equal 5, find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
+  end
+  
+  def test_return_positive_integer_appearing_odd_times
+    assert_equal 5, find_it([20,1,1,2,2,3,3,5,5,4,20,4,5])
+  end
+
+  def test_return_negative_integer_appearing_odd_times
+    assert_equal -1, find_it([1,1,2,-2,5,2,4,4,-1,-2,5])
+  end
+
+  def test_return_nil_if_float_appearing_odd_times
+    assert_equal nil, find_it([1.23, 34, 34])
+  end
+
+  def test_return_integer_if_both_float_and_integer_appearing_odd_times
+    assert_equal nil, find_it([1.23, 1.23, 34, 34])
+  end
+
+  def test_return_integer_if_both_string_and_integer_appearing_odd_times
+    assert_equal nil, find_it([1.23, 1.23, "34", "34", 34, 34])
+  end
+  
 end
