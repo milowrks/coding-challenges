84da590217bd51c040548a49878ade0099cd815b
Milo <milozhengz@gmail.com>
2019-07-19 08:52:18 +1000

    pass basic test assertions

--- is-a-triangle/is_triangle.rb
+++ is-a-triangle/is_triangle.rb
@@ -1,3 +1,9 @@
+# Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+# the sum of the lengths of *any* two sides of a triangle must be greater than or equal to the length of the third side. 
+
 def is_triangle(a,b,c)
-  return true
+  if a + b >= c || a + c >= b || b + c >= a
+    return true
+  end
 end
--- is-a-triangle/is_triangle_test.rb
+++ is-a-triangle/is_triangle_test.rb
@@ -6,7 +6,11 @@ Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 require_relative 'is_triangle'
 
 class IsTriangleTest < MiniTest::Test
-  def test_return_true
-    assert_equal true, is_triangle(1, 2, 3)
+  def test_return_true_if_sum_two_sides_equals_third
+    assert_equal true, is_triangle(3, 2, 1)
+  end
+
+  def test_return_true_if_sum_two_sides_is_longer_than_third
+    assert_equal true, is_triangle(1, 1, 1)
   end
 end
