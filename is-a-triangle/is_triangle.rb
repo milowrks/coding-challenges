98fe30f165da188b27971f971889c7e3f09cdb89
Milo <milozhengz@gmail.com>
2019-07-19 09:06:47 +1000

    test 0 and negative lengths

--- is-a-triangle/is_triangle.rb
+++ is-a-triangle/is_triangle.rb
@@ -3,7 +3,9 @@
 # the sum of the lengths of *any* two sides of a triangle must be greater than or equal to the length of the third side. 
 
 def is_triangle(a,b,c)
-  if a + b >= c || a + c >= b || b + c >= a
+  if (a + b >= c && a + c >= b && b + c >= a) && a > 0 && b > 0 && c > 0
     return true
+  else
+    return false
   end
 end
--- is-a-triangle/is_triangle_test.rb
+++ is-a-triangle/is_triangle_test.rb
@@ -13,4 +13,17 @@ class IsTriangleTest < MiniTest::Test
   def test_return_true_if_sum_two_sides_is_longer_than_third
     assert_equal true, is_triangle(1, 1, 1)
   end
+
+  def test_return_false_if_sum_two_sides_is_shorter_than_third
+    assert_equal false, is_triangle(7, 2, 2)
+  end
+
+  def test_return_false_if_any_side_is_0
+    assert_equal false, is_triangle(0, 2, 2)
+  end
+
+  def test_return_false_if_any_side_is_less_than_0
+    assert_equal false, is_triangle(-2, 4, 2)
+  end
+
 end
