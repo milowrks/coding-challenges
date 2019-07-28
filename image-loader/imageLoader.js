4d7337341d2ea48c13d2ea2b7d1dbf289907dd95
Milo <milozhengz@gmail.com>
2019-07-29 08:28:39 +1000

    solve challenge

--- image-loader/imageLoader.js
+++ image-loader/imageLoader.js
@@ -1,15 +1,11 @@
 async function getImageDimension(image) {
-  // let dimensions = {
-  //   width: 0,
-  //   height: 0
-  // }
   return new Promise (function(resolve, reject) {
     const img = new Image()
     img.onload = function() {
       resolve({ width: this.width, height: this.height });
     }
     img.onerror = function() {
-      reject({ width: 0, height: 0 })
+      resolve({ width: 0, height: 0 })
     }
     img.src = image
   })
@@ -20,31 +16,34 @@ function calcAverageDimensions(dimensions) {
     width: 0,
     height: 0
   }
-  return dimensions.reduce((total, currentImage, index, dimensions) => {
-    total.width += currentImage.width;
-    total.height += currentImage.height;
-    if (index === dimensions.length - 1) {
-      return {
-        width: total.width/dimensions.length,
-        height: total.height/dimensions.length
+  return dimensions
+    .filter(image => image.width > 0)
+    .reduce((total, currentImage, index, dimensions) => {
+      total.width += currentImage.width;
+      total.height += currentImage.height;
+      if (index === dimensions.length - 1) {
+        return {
+          width: total.width/dimensions.length,
+          height: total.height/dimensions.length
+        }
+      } else {
+        return total
       }
-    } else {
-      return total
-    }
-  }, averageDimensions)
+    }, averageDimensions)
 }
 
 async function loadImages(images) {
-  return Promise.all(images.map(image => getImageDimension(image)))
+  const promises = images.map(image => getImageDimension(image))
+  return Promise.all(promises)
+  .then(dimensions => calcAverageDimensions(dimensions))
 }
 
 const imageUrls = [
   'https://www.w3schools.com/html/pic_trulli.jpg',
-  'https://www.w3schools.com/html/img_girl.jpg',
+  'https://www.w3schools.com/html/img_girrl.jpg',
   'https://www.w3schools.com/html/img_chania.jpg',
   'https://www.w3schools.com/html/workplace.jpg',
   'https://www.w3schools.com/w3css/img_snowtops.jpg',		
 ]
 
-console.log(calcAverageDimensions([{width: 500, height: 300}, {width: 400, height: 200}]))
 console.log(loadImages(imageUrls))
