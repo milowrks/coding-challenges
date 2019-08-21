0746ba181b7e804bd7223fa44d8d8922c3c537f0
Milo <milozhengz@gmail.com>
2019-08-21 11:22:52 +1000

    add pseudocode

--- money-money-money/money_money_money.rb
+++ money-money-money/money_money_money.rb
@@ -1,3 +1,6 @@
 def calculate_years(principal, interest, tax, desired)
-  return true
+  # while loop - exit when sum > desired
+    # calculate sum after one year minus tax
+    # reassign sum to principal and calculate
+  # return years (num of loops)
 end
