b967d44fe29b4c405eab3f4e72c78f443f9ca746
Milo <milozhengz@gmail.com>
2019-08-21 11:49:18 +1000

    works for non-edge cases

--- money-money-money/money_money_money.rb
+++ money-money-money/money_money_money.rb
@@ -1,6 +1,22 @@
+require 'pry'
+
 def calculate_years(principal, interest, tax, desired)
-  # while loop - exit when sum > desired
-    # calculate sum after one year minus tax
-    # reassign sum to principal and calculate
-  # return years (num of loops)
+  years = 0
+  sum = 0
+  # loop do
+  #   years+=1
+  #   sum = principal * (1 + interest) * (1 - tax)
+  #   principal = sum
+  #   if principal >= desired
+  #     break
+  #   end
+  # end
+  while principal <= desired do
+    years+=1
+    principal = principal + principal * interest * (1 - tax)
+    # binding.pry
+  end
+  return years
 end
+
+puts calculate_years(1000, 0.05, 0.18, 1100)
--- money-money-money/money_money_money_test.rb
+++ money-money-money/money_money_money_test.rb
@@ -6,7 +6,10 @@ Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 require_relative 'money_money_money'
 
 class MoneyMoneyMoneyTest < MiniTest::Test
-  def test_works
-    assert_equal true, calculate_years(0, 0, 0, 0)
+  def test_should_return_correct_years
+    assert_equal 3, calculate_years(1000, 0.05, 0.18, 1100)
+  end
+  def test_should_return_0_if_desired_amount_equals_principal
+    assert_equal 0, calculate_years(1000, 0.05, 0.18, 1000)
   end
 end
