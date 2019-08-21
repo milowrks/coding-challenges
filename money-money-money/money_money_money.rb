c5ec9ffcec7c01faf3058c60305f93631199461e
Milo <milozhengz@gmail.com>
2019-08-21 11:57:13 +1000

    solve and log challenge

--- money-money-money/money_money_money.rb
+++ money-money-money/money_money_money.rb
@@ -1,22 +1,9 @@
-require 'pry'
-
 def calculate_years(principal, interest, tax, desired)
   years = 0
   sum = 0
-  # loop do
-  #   years+=1
-  #   sum = principal * (1 + interest) * (1 - tax)
-  #   principal = sum
-  #   if principal >= desired
-  #     break
-  #   end
-  # end
-  while principal <= desired do
+  while principal < desired do
     years+=1
     principal = principal + principal * interest * (1 - tax)
-    # binding.pry
   end
   return years
 end
-
-puts calculate_years(1000, 0.05, 0.18, 1100)
