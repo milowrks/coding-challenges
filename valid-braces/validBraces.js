755c5f293baad3347678ecf89f430488239296c8
Milo <milozhengz@gmail.com>
2019-08-23 12:11:52 +1000

    set up challenge and write pseudocode

---
+++ valid-braces/validBraces.js
@@ -0,0 +1,11 @@
+function validBraces(braces){
+  // define 3 types of braces
+  // check if string includes any of the opening braces
+    // yes => get index numbers of the opening brace and matching closing brace
+      // check if number of opening and closing braces match
+        // yes => sort index numbers and check if each opening brace index < closing brace index at position
+          // yes => return true
+          // no => return false
+        // no => return false
+    // no => return false
+}
