e341807c661bbb1d4077135dcdb9f6d18f99ecc3
Milo <milozhengz@gmail.com>
2019-08-23 12:36:30 +1000

    write methods to get position of opening and closing braces

--- valid-braces/validBraces.js
+++ valid-braces/validBraces.js
@@ -1,11 +1,33 @@
 function validBraces(braces){
   // define 3 types of braces
-  // check if string includes any of the opening braces
-    // yes => get index numbers of the opening brace and matching closing brace
-      // check if number of opening and closing braces match
-        // yes => sort index numbers and check if each opening brace index < closing brace index at position
-          // yes => return true
+  const typesOfBraces = [["(", ")"], ["{", "}"], ["[", "]"]];
+  typesOfBraces.forEach(type => {
+    // check if string includes any of the opening braces
+    if (braces.includes(type[0])) {
+      // yes => get index numbers of the opening brace and matching closing brace
+        const openBrace = type[0];
+        const closeBrace = type[1];
+        const openBraceIndexes = 
+          braces.split("")
+          .reduce((arr, elem, i) => {
+            elem === openBrace && arr.push(i);
+            return arr;
+          }, []);
+        const closeBraceIndexes =
+          braces.split("")
+          .reduce((arr, elem, i) => {
+            elem === closeBrace && arr.push(i);
+            return arr;
+          }, []);
+        debugger
+        // check if number of opening and closing braces match
+          // yes => sort index numbers and check if each opening brace index < closing brace index at position
+            // yes => return true
+            // no => return false
           // no => return false
-        // no => return false
-    // no => return false
+    } else {
+      // no => return false
+      return false;
+    }
+  })
 }
