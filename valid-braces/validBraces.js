95b9758748ff217b2224dc9edfbee04aa9cb91a3
Milo <milozhengz@gmail.com>
2019-08-23 13:52:33 +1000

    solution complete - need to re-design validation logic

--- valid-braces/validBraces.js
+++ valid-braces/validBraces.js
@@ -1,33 +1,47 @@
 function validBraces(braces){
   // define 3 types of braces
   const typesOfBraces = [["(", ")"], ["{", "}"], ["[", "]"]];
-  typesOfBraces.forEach(type => {
+  const results = typesOfBraces.map(type => {
     // check if string includes any of the opening braces
     if (braces.includes(type[0])) {
       // yes => get index numbers of the opening brace and matching closing brace
         const openBrace = type[0];
         const closeBrace = type[1];
-        const openBraceIndexes = 
-          braces.split("")
+        const openBraceIndexes = braces
+          .split("")
           .reduce((arr, elem, i) => {
             elem === openBrace && arr.push(i);
             return arr;
-          }, []);
-        const closeBraceIndexes =
-          braces.split("")
+          }, [])
+          .sort();
+        const closeBraceIndexes =braces
+          .split("")
           .reduce((arr, elem, i) => {
             elem === closeBrace && arr.push(i);
             return arr;
-          }, []);
-        debugger
-        // check if number of opening and closing braces match
+          }, [])
+          .sort();
+          // check if number of opening and closing braces match
+        if (openBraceIndexes.length === closeBraceIndexes.length) {
           // yes => sort index numbers and check if each opening brace index < closing brace index at position
-            // yes => return true
-            // no => return false
+          for (let i = 0; i < openBraceIndexes.length; i++) {
+            if (openBraceIndexes[i] < closeBraceIndexes[i]) {
+              // yes => return true
+              return true;
+            } else {
+              // no => return false
+              return false;
+            }
+          }
+        } else {
           // no => return false
-    } else {
+          return false;
+        }
+    } else if (braces.includes(type[1])) {
       // no => return false
       return false;
     }
   })
+  debugger
+  return results.filter(result => result === false).length === 0;
 }
