32859276a9a986a75f6f8e0d5260b3640f555a08
Milo <milozhengz@gmail.com>
2019-09-12 09:18:39 +1000

    set up challenge and test

---
+++ list-filtering/listFiltering.js
@@ -0,0 +1,6 @@
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return 'it works'
+}
+
+module.exports = filter_list
---
+++ list-filtering/listFiltering.test.js
@@ -0,0 +1,5 @@
+const filter_list = require('./listFiltering');
+
+test('it works', () => {
+  expect(filter_list()).toBe("it works");
+});
